bf49136a4e4acb139e96713e288a1ba0b049a035
Ell Harman <78617707+ellharman@users.noreply.github.com>
2022-05-06 00:53:36 +0100

    Add homepage text and nav

---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="navbar bg-primary">
+    <div class="flex-1 flex-wrap">
+      <NuxtLink class="btn btn-ghost normal-case text-xl" to="/">Home</NuxtLink>
+      <NuxtLink class="btn btn-ghost normal-case text-xl" to="/about"
+        >About</NuxtLink
+      >
+      <NuxtLink class="btn btn-ghost normal-case text-xl" to="/events"
+        >Events</NuxtLink
+      >
+      <NuxtLink class="btn btn-ghost normal-case text-xl" to="/photos"
+        >Photos</NuxtLink
+      >
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="flex h-screen bg-black text-white content-center">
-    <div class="m-auto"><UnderConstruction /></div>
+  <div
+    class="flex flex-col h-screen bg-black text-white content-center overflow-auto scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100"
+  >
+    <!-- <div class="m-auto"><UnderConstruction /></div> -->
+    <div class="w-screen"><NavBar /></div>
+    <div class="mx-auto"><SlaystationLogo /></div>
+    <div class="mx-auto w-screen px-96">
+      <p class="font-mono text-4xl text-justify w-auto pb-20">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere
+        tempore itaque sunt aut voluptatem modi aliquid ea. Praesentium vero
+        enim quae libero voluptatibus laboriosam odio consectetur et eum sed!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+        quod non quas, pariatur error labore voluptatibus? Explicabo, quam,
+        labore corporis distinctio quis sequi modi asperiores optio libero cum
+        impedit fuga! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        Voluptate, consequatur! Asperiores minus, officia impedit placeat, nihil
+        consectetur quisquam magni numquam ullam, perferendis voluptate dicta
+        voluptas eligendi provident natus expedita quas?
+      </p>
+    </div>
   </div>
 </template>
 
