8a99f1344744ad00af21dcc262442f84658baeea
Ell Harman <78617707+ellharman@users.noreply.github.com>
2022-05-19 17:42:04 +0100

    Nav padding

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="navbar bg-primary rounded">
-    <div class="flex items-center grow">
+    <div class="flex flex-wrap items-center grow">
       <NuxtLink
-        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl"
+        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl px-2 md:px-4"
         to="/"
         >Home</NuxtLink
       >
       <NuxtLink
-        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl"
+        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl px-2 md:px-4"
         to="/about"
         >About</NuxtLink
       >
       <NuxtLink
-        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl"
+        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl px-2 md:px-4"
         to="/events"
         >Events</NuxtLink
       >
       <NuxtLink
-        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl"
+        class="flex-auto btn btn-ghost normal-case text-navFont md:text-xl px-2 md:px-4"
         to="/photos"
         >Photos</NuxtLink
       >
