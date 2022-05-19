0d9996b0ad5d0490e02c06e65d02c7b293280e09
Ell Harman <78617707+ellharman@users.noreply.github.com>
2022-05-19 15:57:37 +0100

    Add index text and mobile fixes

--- components/SlaystationLogo.vue
+++ components/SlaystationLogo.vue
@@ -3,5 +3,6 @@
     src="/slaystation_logo.png"
     alt="Logo for Slaystation"
     sizes="sm:100vw md:50vw lg:400px"
+    class="mx-auto"
   />
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,26 @@
 <template>
-  <div>
+  <div class="md:w-1/2">
     <!-- <div class="m-auto"><UnderConstruction /></div> -->
-    <div class="w-1/2"><SlaystationLogo class="mx-auto" /></div>
-    <div class="w-1/2 px-16">
-      <p class="font-mono text-3xl text-justify w-auto">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore facere
-        tempore itaque sunt aut voluptatem modi aliquid ea. Praesentium vero
-        enim quae libero voluptatibus laboriosam odio consectetur et eum sed!
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        quod non quas, pariatur error labore voluptatibus? Explicabo, quam,
-        labore corporis distinctio quis sequi modi asperiores optio libero cum
-        impedit fuga! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Voluptate, consequatur! Asperiores minus, officia impedit placeat, nihil
-        consectetur quisquam magni numquam ullam, perferendis voluptate dicta
-        voluptas eligendi provident natus expedita quas?
+    <div class="mx-auto my-10"><SlaystationLogo /></div>
+    <div class="px-16 pb-10">
+      <p class="font-mono text-2xl text-justify w-auto">
+        SlayStation is the new London night celebrating gaming and the space it
+        has provided queers to explore their identities. Now it's time to take
+        that space IRL with performances, games, costumes, music, and prizes
+        themed after your favourite series and moments.
+      </p>
+      <br />
+      <p class="font-mono text-2xl text-justify w-auto">
+        Since beginning at the end of 2021, we have had successful monthly shows
+        covering different gaming genres and series, ranging from Sci-Fi to
+        Fantasy, Fighting to Pokémon. We are committed to providing a safe space
+        for all people to express themselves and have fun and we do this with an
+        experience like no other in the world.
+      </p>
+      <br />
+      <p class="font-mono text-2xl text-justify w-auto">
+        So if you want to see a Solid Snake strip tease, glitchy Elder Scrolls
+        parodies, or sexy Waluigi, this is the night for you!
       </p>
     </div>
   </div>
